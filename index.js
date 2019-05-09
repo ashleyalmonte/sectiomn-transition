c63e48984c344c5c92a892720afad1f9fe389105
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-09 15:09:07 -0400

    idk what to do with

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-get section = document.getElementsByTagName('section')
+   let div1 = document.getElementById("div1")
+   window.addEventListner("scroll", funcition() {
+   console.log(window.scrollY)
+   if(window.scrollY > 0) {
+   div1.classList.remove("hidden")
+   div1.classList.add("show")
+ }
+  })
 
-let divs = document.getElementsByTagName('div')
-
-window.addEventListner('scroll', function(event) {
-  console.log(window.scrollY)
-  if(window.scrollY > 500) {
-
-    for (div of divs) {
-      div.classList.remove('hidden')
-      div.classList.add('show')
+  let div2 = document.getElementById("div2")
+  window.addEventListner("scroll", function(){
+    console.log(window.scrollY);
+    if(window.scrollY)
+    if(window.scrollY > 800) {
+      div2.classList.remove("hidden")
+      div2.classList.add("show")
     }
-  }
-})
-
-console.log(section)
+  })
